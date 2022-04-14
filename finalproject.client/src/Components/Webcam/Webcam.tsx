dcb15e7241850a46ef38bf4d0d1dddb0507541d3
Alp0x <gron141@gmail.com>
2022-04-14 22:25:20 +0200

    Removed photo dependency

--- finalproject.client/src/Components/Webcam/Webcam.tsx
+++ finalproject.client/src/Components/Webcam/Webcam.tsx
@@ -27,7 +27,7 @@ const Capture = () => {
             }).then(res => console.log(res)).catch(console.error);
         }
         postPhoto();
-    }, [WebcamRef, setPhoto, photo, userId]);
+    }, [WebcamRef, setPhoto, userId]);
     
     useEffect(() =>{
        
